0c0f14320d76154079f9eeffc3b960c0127ec268
Ngô Hào Nam <ngohaonam90@gmail.com>
2024-10-29 09:04:19 +0700

    22674931

---
+++ STT43_NgoHaoNam_22674931_24/js/jquery.js
@@ -0,0 +1,186 @@
+$(document).ready(function() {
+    $("#mo").click(function(e) {
+        $("#molencoi").modal();
+
+    });
+    $("#vien").val("K16-123456789");
+    $("#ten").val("Tran Anh Dung");
+    $("#lop").val("DHKTPM16BTT");
+    $("#so").val("0xxx-xxx-xxx ");
+    $("#meo").val("xxxxx@iuh.edu.vn ");
+
+
+    function validatevien() {
+        let vien = $("#vien").val();
+        let validatevien = /^K16\-\d{9}$/;
+        if (vien === "") {
+            $("#loivien").html("Bat buoc nhap");
+            return false;
+        } else {
+            if (!validatevien.test(vien)) {
+                $("#loivien").html("Bao loi");
+                return false;
+            } else {
+                $("#loivien").html("*");
+                return true;
+            }
+        }
+
+    }
+
+
+    function validateten() {
+        let ten = $("#ten").val();
+        let validateten = /^[A-Z][a-z]+(\s[A-Z][a-z]+)+$/;
+        if (ten === "") {
+            $("#loiten").html("Bat buoc nhap");
+            return false;
+        } else {
+            if (!validateten.test(ten)) {
+                $("#loiten").html("Bao loi");
+                return false;
+            } else {
+                $("#loiten").html("*");
+                return true;
+            }
+        }
+
+    }
+
+    function validatelop() {
+        let lop = $("#lop").val();
+        let validatelop = /^[A-Z0-9]{11}$/;
+        if (lop === "") {
+            $("#loilop").html("Bat buoc nhap");
+            return false;
+        } else {
+            if (!validatelop.test(lop)) {
+                $("#loilop").html("Bao loi");
+                return false;
+            } else {
+                $("#loilop").html("*");
+                return true;
+            }
+        }
+
+    }
+
+    function validateso() {
+        let so = $("#so").val();
+        let validateso = /^0[0-9]{3}\-[0-9]{3}\-[0-9]{3}$/;
+        if (so === "") {
+            $("#loiso").html("Bat buoc nhap");
+            return false;
+        } else {
+            if (!validateso.test(so)) {
+                $("#loiso").html("Bao loi");
+                return false;
+            } else {
+                $("#loiso").html("*");
+                return true;
+            }
+        }
+
+    }
+
+    function validatemeo() {
+        let meo = $("#meo").val();
+        let validatemeo = /^0[0-9]+\@iuh\.edu\.vn$/;
+        if (meo === "") {
+            $("#loimeo").html("Bat buoc nhap");
+            return false;
+        } else {
+            if (!validatemeo.test(meo)) {
+                $("#loimeo").html("Bao loi");
+                return false;
+            } else {
+                $("#loimeo").html("*");
+                return true;
+            }
+        }
+
+    }
+
+    function validatengay() {
+        let ngay = $("#ngay").val();
+        let ngayht = new Date(ngay);
+        let ngayhientai = new Date();
+        let sau30ngay = new Date(ngayhientai);
+        sau30ngay.setDate(ngayhientai.getDate() + 30);
+        if (ngay === "") {
+            $("#loingay").html("Bat buoc nhap");
+            return false;
+        } else {
+            if (ngayht < sau30ngay) {
+                $("#loingay").html("Ngày tham gia sau ngày hiện tại 30 ngày");
+                return false;
+            } else {
+                $("#loingay").html("*");
+                return true;
+            }
+        }
+
+
+
+
+
+
+
+    }
+
+    let i = 1;
+
+    $("#vien").blur(validatevien);
+    $("#ten").blur(validateten);
+    $("#lop").blur(validatelop);
+    $("#so").blur(validateso);
+    $("#meo").blur(validatemeo);
+    $("#ngay").blur(validatengay);
+
+
+    $("#gui").click(function(e) {
+        e.preventDefault();
+        let vien = $("#vien").val();
+        let ten = $("#ten").val();
+        let lop = $("#lop").val();
+        let so = $("#so").val();
+        let meo = $("#meo").val();
+        let ngay = $("#ngay").val();
+
+
+        let row = " <tr> <td>" + i + "</td>           <td>" + vien + "</td>     <td>" + ten + "</td>     <td>" + lop + "</td>     <td>" + ngay + "</td>     <td>" + so + "</td>     <td>" + meo + "</td>                                                                      </tr>";
+        $("#than").append(row);
+        i++;
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
